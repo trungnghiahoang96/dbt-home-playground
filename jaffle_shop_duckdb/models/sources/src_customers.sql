04b462b3c4a03cf7c61a099039928f1fb33c63c1
Trung Nghĩa Hoàng <46636991+trungnghiahoang96@users.noreply.github.com>
2023-06-18 22:24:44 +0700

    Refactor to have source

---
+++ jaffle_shop_duckdb/models/sources/src_customers.sql
@@ -0,0 +1,12 @@
+with source as (
+
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+    select * from {{ ref('raw_customers') }}
+
+)
+
+
+select * from source
